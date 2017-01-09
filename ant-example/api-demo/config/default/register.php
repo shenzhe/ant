1e14129a1c90e90344559d8ebc5a574f6747c417
shenzhe <shenzhe163@gmail.com>
2017-01-09 20:39:01 +0800

    添加demo

---
+++ ant-example/api-demo/config/default/register.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shenzhe
+ * Date: 2016/11/29
+ * Time: 11:21
+ */
+use common\Consts;
+
+return array(
+    'soa' => array(
+        'ip' => '10.94.107.22',
+        'port' => 9949,
+        'timeOut' => 5000
+    ),
+);
---
+++ example/demo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shenzhe
+ * Date: 2017/1/9
+ * Time: 20:25
+ */
+require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'sdk' . DIRECTORY_SEPARATOR . 'TcpClient.php';
+
+$service = new \TcpClient('10.94.107.22', 7101, 1500);
+echo $service->call('test');
