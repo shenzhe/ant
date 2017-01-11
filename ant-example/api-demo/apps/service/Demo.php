843668027540b9e680bdb12d2bd4e7cc33866b16
shenzhe <shenzhe163@gmail.com>
2017-01-11 21:27:37 +0800

    注释

--- ant-example/api-demo/apps/service/Demo.php
+++ ant-example/api-demo/apps/service/Demo.php
@@ -12,12 +12,15 @@ use sdk\LoadService;
 
 class Demo
 {
-    public function demo($method)
+    public function demo($method, $params=[])
     {
-        //获取一个远程服务
+        //获取服务名为 api-demo2 的一个远程服务,
         $service = LoadService::getService('api-demo2');
         //执行服务的方法
-        $result = $service->call($method);
+        $result = $service->call($method, $params);
+        //也可以这么执行
+        //$result = $service->{$method}($params);
+        //$result = $service->main($params);
         //获取结果
         $body = $result->getBody();
         return $body['data'];
