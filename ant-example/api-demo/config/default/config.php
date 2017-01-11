f080fdcbf4b21c5ae3b1b9550655bdbd382359e8
shenzhe <shenzhe163@gmail.com>
2017-01-11 19:18:52 +0800

    demo default config

--- ant-example/api-demo/config/default/config.php
+++ ant-example/api-demo/config/default/config.php
@@ -1,13 +1,20 @@
 <?php
-
-    return array(
-        'server_mode' => (PHP_SAPI === 'cli') ? 'Cli' : 'Http',
-        'app_path'=>'apps',
-        'ctrl_path'=>'ctrl',
-        'project'=>array(
-            'name'=>'api-demo',                 
-        	'view_mode'=>'Str',   		
-        	'ctrl_name'=>'a',				
-        	'method_name'=>'m',				
-        )
-    );
+use ZPHP\ZPHP;
+return array(
+    'server_mode' => (PHP_SAPI === 'cli') ? 'Cli' : 'Http',
+    'app_path'=>'apps',
+    'ctrl_path'=>'ctrl',
+    'lib_path' => array(
+        'ant-lib' => ZPHP::getRootPath() . DS . '..' . DS . '..' . DS . 'ant-lib',
+        'ant-rpc' => ZPHP::getRootPath() . DS . '..' . DS . '..' . DS . 'ant-rpc',
+    ),
+    'project' => [
+        'default_ctrl_name' => 'main',                      //默认入口控制器
+        'debug_mode' => 0,                                  //打开调试模式
+        'protocol' => 'Ant',
+        'view_mode' => 'Ant',
+        'exception_handler' => 'common\MyException::exceptionHandler',
+        'fatal_handler' => 'common\MyException::fatalHandler',
+        'error_handler' => 'common\MyException::errorHandler',
+    ],
+);
