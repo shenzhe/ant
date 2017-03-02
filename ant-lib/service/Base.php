b0a8c13a4fe1cbd2ff7ebf9e5104b9b789778db9
shenzhe <shenzhe163@gmail.com>
2017-03-02 19:18:07 +0800

    获取子类名

--- ant-lib/service/Base.php
+++ ant-lib/service/Base.php
@@ -9,7 +9,7 @@ abstract class Base
 {
     public function __call($name, $arguments)
     {
-        $dao = LoadClass::getDao($name);
+        $dao = LoadClass::getDao(substr(get_called_class(), 8));
         if (method_exists($dao, $name)) {
             return call_user_func_array([$dao, $name], $arguments);
         }
